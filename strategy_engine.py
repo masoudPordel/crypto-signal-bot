e304a11e4c05e78a3e5da3556f2c004a0d711e7d
masoudPordel <masoudpordel@gmail.com>
2025-04-18 10:27:50 +0330

    Delete strategy_engine.py

--- strategy_engine.py
+++
@@ -1,6 +0,0 @@
-from analyzer import scan_all_symbols, scan_forex_symbols
-
-def generate_signals():
-    crypto_signals = scan_all_symbols()
-    forex_signals = scan_forex_symbols()
-    return crypto_signals + forex_signals
