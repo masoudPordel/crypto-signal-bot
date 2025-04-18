3416a34cbf9812d3ffe012ff0ad308ccea924570
masoudPordel <masoudpordel@gmail.com>
2025-04-18 17:01:49 +0330

    strategy_engine.py

--- strategy_engine.py
+++ strategy_engine.py
@@ -1,15 +1,7 @@
 from analyzer import scan_all_crypto_symbols, scan_all_forex_symbols
 
 def generate_crypto_signals():
-    crypto_signals = scan_all_crypto_symbols()
-    signals = []
-    for signal in crypto_signals:
-        signals.append(f"Symbol: {signal['symbol']}\nEntry: {signal['entry']}\nTP: {signal['tp']}\nSL: {signal['sl']}\nTF: {signal['tf']}")
-    return signals
+return scan_all_crypto_symbols()
 
 def generate_forex_signals():
-    forex_signals = scan_all_forex_symbols()
-    signals = []
-    for signal in forex_signals:
-        signals.append(f"Symbol: {signal['symbol']}\nEntry: {signal['entry']}\nTP: {signal['tp']}\nSL: {signal['sl']}\nTF: {signal['tf']}")
-    return signals
+return scan_all_forex_symbols()
