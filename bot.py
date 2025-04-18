cf1035ddb7a61ca39cad8963fec20a515aba9f0a
masoudPordel <masoudpordel@gmail.com>
2025-04-18 12:05:30 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,28 @@
 import os
+
+import asyncio
+
 from telegram import Bot
 
+
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 USER_ID = 632886964  # Masoud Pordel
 
+
+
 bot = Bot(token=BOT_TOKEN)
-bot.send_message(chat_id=USER_ID, text="✅ ربات وصله و کار می‌کنه!")
+
+
+
+async def main():
+
+    await bot.send_message(chat_id=USER_ID, text="✅ ربات وصله و کار می‌کنه!")
+
+
+
+if __name__ == "__main__":
+
+    asyncio.run(main())
+
