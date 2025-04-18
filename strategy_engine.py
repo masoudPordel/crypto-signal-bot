98913d910c9c54f183244431a5e8cf45b0f6283e
masoudPordel <masoudpordel@gmail.com>
2025-04-18 15:30:48 +0330

    Delete strategy_engine.py

--- strategy_engine.py
+++
@@ -1,7 +0,0 @@
-from analyzer import scan_all_crypto_symbols, scan_all_forex_symbols
-
-def generate_crypto_signals():
-return scan_all_crypto_symbols()
-
-def generate_forex_signals():
-return scan_all_forex_symbols()
