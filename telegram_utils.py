5bc22808ebe2e910f725ee4afcf3ce906ad69a9a
masoudPordel <masoudpordel@gmail.com>
2025-04-19 17:38:49 +0330

    Delete telegram_utils.py

--- telegram_utils.py
+++
@@ -1,8 +0,0 @@
-from telegram import Bot
-
-BOT_TOKEN = "8111192844:AAHuVZYs6RolBhdqPpTWW9g7ksGRaq3p0WA"
-CHAT_ID = 632886964
-
-def send_message(text):
-    bot = Bot(token=BOT_TOKEN)
-    bot.send_message(chat_id=CHAT_ID, text=text)
