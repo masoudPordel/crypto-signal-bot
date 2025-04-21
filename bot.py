34b18bcf0f3acc4fbce8c842189a36dc7424c833
masoudPordel <masoudpordel@gmail.com>
2025-04-21 14:47:15 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ logging.basicConfig(
     force=True
 )
 
-BOT_TOKEN = "7914790659:AAGQenCqPQQyRUbBJDR0CH_wG_s7rluT2_I"
+BOT_TOKEN = "8111192844:AAHuVZYs6RolBhdqPpTWW9g7ksGRaq3p0WA"
 CHAT_ID = 632886964
 
 LOCK_FILE = "bot.lock"
