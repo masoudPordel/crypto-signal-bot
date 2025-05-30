ea4dcf09deeca8126b3325a7027d7ea3ce262d96
masoudPordel <masoudpordel@gmail.com>
2025-05-31 01:43:50 +0330

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -256,6 +256,7 @@ class PatternDetector:
                         bearish_divergence = (last_price_high > prev_price_high * 0.98) and (last_rsi_high < prev_rsi_high * 1.02)
                 return bullish_divergence, bearish_divergence
                 
+class IndicatorCalculator:
         @staticmethod
         def compute_moving_averages(df: pd.DataFrame, window_short: int = 9, window_long: int = 21) -> pd.DataFrame:
                 df['ma_short'] = df['close'].rolling(window=window_short).mean()
