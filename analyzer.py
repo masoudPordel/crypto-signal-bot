46ce773e22dfc4ed39896df5a87342b94ca593f2
masoudPordel <masoudpordel@gmail.com>
2025-05-31 20:53:59 +0330

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -27,7 +27,7 @@ logging.basicConfig(
 )
 
 # تنظیمات ثابت
-CMC_API_KEY = os.getenv("CMC_API_KEY", "ff5a7b31-458f-4e5a-a684-acba34c33c11")
+CMC_API_KEY = os.getenv("CMC_API_KEY", "3bd94b7b-281b-468e-94d7-8449c379d67f")
 COINMARKETCAL_API_KEY = os.getenv("COINMARKETCAL_API_KEY", "iFrSo3PUBJ36P8ZnEIBMvakO5JutSIU1XJvG7ALa")
 TIMEFRAMES = ["15m", "1h", "4h", "1d"]
 
